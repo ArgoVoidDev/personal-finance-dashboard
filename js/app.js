737319cb71b1a1aa5e1a26583f9dd6b912763bb6
ArgoVoid <argovoid.studio@gmail.com>
2026-06-28 19:11:42 +0330

    feat: add navigation system between pages

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const pageTitles = {
+  dashboard: "Dashboard",
+  transactions: "Transactions",
+  budget: "Budget",
+  savings: "Savings",
+  settings: "Settings",
+};
+const pages = document.querySelectorAll(".page");
+const btns = document.querySelectorAll(".sidebar__nav-btn");
+const pageTitle = document.querySelector("#page-title");
+
+function navigateTo(pageId) {
+  pages.forEach((page) => page.classList.remove("page--active"));
+
+  const activePage = document.querySelector("#" + pageId);
+  activePage.classList.add("page--active");
+
+  btns.forEach((btn) => btn.classList.remove("sidebar__nav-btn--active"));
+
+  const activeBtn = document.querySelector(`[data-page="${pageId}"]`);
+
+  activeBtn.classList.add("sidebar__nav-btn--active");
+
+  pageTitle.textContent = pageTitles[pageId];
+}
+btns.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const pageId = button.dataset.page;
+
+    navigateTo(pageId);
+  });
+});
+navigateTo("dashboard");
